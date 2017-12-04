412b972b13a2b22db0bea99690d35ed976736161
Vincent Courtois <babaussine@gmail.com>
2017-12-04 14:32:27 +0100

    Migrate the dependencies on bower

--- client/tasks/build.js
+++ client/tasks/build.js
@@ -4,6 +4,8 @@ const babelify = require('babelify')
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
 const watchify = require('watchify')
+const strip = require('gulp-strip-comments')
+const streamify = require('gulp-streamify')
 
 const browserifyOptions = assign({}, watchify.args, {entries: ['src/app.js'], debug: true})
 const babelOptions = {presets: ['es2015']}
@@ -14,5 +16,6 @@ instance.transform(babelify.configure(babelOptions))
 gulp.task('build', function () {
   return instance.bundle()
     .pipe(source('app.js'))
+    .pipe(streamify(strip()))
     .pipe(gulp.dest('dist'))
 })
