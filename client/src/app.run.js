26c5dcbcc3e925048bf1679ad20cd352974cd49e
Babausse <courtois.vincent@outlook.com>
2018-11-21 23:29:36 +0100

    Fix #80 fix the height of the viewport on mobile devices.

--- client/src/app.run.js
+++ client/src/app.run.js
@@ -1,6 +1,15 @@
-const running = function runningFunction ($localStorage, WebsocketChannel) {
+const running = function runningFunction ($document, $localStorage, $window, WebsocketChannel) {
   'ngInject'
   if ($localStorage.token !== undefined) WebsocketChannel.setup()
+
+  const resizeMainContainer = () => {
+    $window.jQuery('.layout-container').height(`${$window.innerHeight}px`)
+  }
+
+  /** This line resets the height of the container to better handle mobile top bars. */
+  $window.jQuery($document).ready(resizeMainContainer)
+  /** The height of the main container should be resetted when resizing it. */
+  $window.jQuery($window).resize(resizeMainContainer)
 }
 
 export default running
